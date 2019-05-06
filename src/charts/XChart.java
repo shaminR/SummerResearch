f1571f7aefc7d4cd2dcc60a6c8fb00ff51798787
shamin <shamin99@outlook.com>
2019-05-05 20:49:24 -0600

    deleted old code

--- src/charts/XChart.java
+++ src/charts/XChart.java
@@ -20,8 +20,6 @@ import org.knowm.xchart.style.markers.SeriesMarkers;
 /** Creates a simple real-time chart */
 public class XChart {
 
-	// double[] xData;
-	// double[] yData;
 	List<Double> xData;
 	List<Double> yData;
 	double[][] graphData;
@@ -30,7 +28,6 @@ public class XChart {
 
 	public XYChart makeLineGraph() throws Exception {
 		System.setProperty("xchart.nosys", "true");
-		// chart = QuickChart.getChart("A line graph", "Time", "Voltage", "sensor voltage", graphData[0], graphData[1]);
 		chart = new XYChartBuilder().width(800).height(600).theme(ChartTheme.Matlab).title("voltage over time").xAxisTitle("time (s)").yAxisTitle("Voltage (V)").build();
 
 		xData = new ArrayList<Double>(20);
@@ -53,20 +50,16 @@ public class XChart {
 		chart.getStyler().setLegendPosition(LegendPosition.InsideNE);		
 		chart.getStyler().setYAxisMin(0.0);
 		chart.getStyler().setYAxisMax(100.0);		
-		chart.getStyler().setXAxisMin(xData.get(0)-1.0);
+		chart.getStyler().setXAxisMin(xData.get(0)+1.0);
 	}
 
 	public void runChart(JPanel panel)throws Exception{
 		while(true){
 			Thread.sleep(200);
-
-			// graphData = appendedGraph(graphData);
 			update();
 			chart.updateXYSeries("sensor voltages", xData, yData, null);//, arg3)
-			// chart.updateXYSeries("sensor voltage", graphData[0], graphData[1], null);
 			panel.revalidate();
 			panel.repaint();
-			// sw.repaintChart();
 		}
 	}
 
@@ -77,25 +70,11 @@ public class XChart {
 
 		yData.remove(0);
 		yData.add(randomGenerator());
-	}
-
-	@Deprecated
-	private double[][] appendedGraph(double[][] arr) {
-		double[] xDataNew = new double[arr[0].length];
-		double[] yDataNew = new double[arr[1].length];
-		int i;
-		for(i = 1; i < arr[0].length; i++){
-			xDataNew[i-1] = arr[0][i];
-			yDataNew[i-1] = arr[1][i];
-		}
-		xDataNew[arr[0].length-1] = arr[0][arr[0].length-1] + 1;
-		yDataNew[arr[1].length-1] = randomGenerator();
-
-		return new double[][] {xDataNew, yDataNew};
+		chart.getStyler().setXAxisMin(xData.get(0)+1.0);
 	}
 
 	public double randomGenerator() {
-		int[] choices = {20, 30, 40};
+		int[] choices = {20, 30, 40, 50, 60};
 		int index = new Random().nextInt(choices.length);
 		return (double)choices[index];
 	}
